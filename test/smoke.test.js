9cbcb902166f74377996600225e9a3d0c08c85fa
pawurb <contact@pawelurbanek.com>
2020-08-05 18:09:25 +0200

    ascii table

--- test/smoke.test.js
+++ test/smoke.test.js
@@ -2,13 +2,22 @@ const PostgresExtras = require("../lib/postgres-extras").PostgresExtras
 
 const database_url = process.env.DATABASE_URL || "postgresql://postgres:secretpass097321@localhost:5432/node-postgres-extras"
 
-test('query succedeed', (done) => {
+test('query in raw format', (done) => {
   PostgresExtras.query('cache_hit', {
       database_url,
       format: 'raw'
     }
   ).then((result) => {
     expect(result.rows[0].name).toBe('index hit rate')
+    done()
+  })
+});
+
+test('query in ascii format', (done) => {
+  PostgresExtras.query('cache_hit', {
+      database_url,
+    }
+  ).then(() => {
     done()
   })
 });
